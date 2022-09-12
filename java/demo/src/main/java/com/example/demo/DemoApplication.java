bcd83db6c718f03b213d2a4934fb7b3db33c2ec7
jyang <did3706@naver.com>
2022-09-12 18:15:12 +0900

    demo 브랜치 추가

--- java/demo/src/main/java/com/example/demo/DemoApplication.java
+++ java/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
+        System.out.println("Hello demo");
+        System.out.println("demo 브랜치 추가");
         SpringApplication.run(DemoApplication.class, args);
     }
 }
