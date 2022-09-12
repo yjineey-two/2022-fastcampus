c5d614b9ab97fba9e1d85ebbc4a0c4a6f96e7117
jyang <did3706@naver.com>
2022-09-12 18:17:49 +0900

    todo 테스트

--- java/todo-server/src/main/java/com/example/todoserver/TodoServerApplication.java
+++ java/todo-server/src/main/java/com/example/todoserver/TodoServerApplication.java
@@ -3,6 +3,6 @@ package com.example.todoserver;
 public class TodoServerApplication {
     public static void main(String[] args) {
         System.out.println("HELLO TODO");
-        System.out.println("TODO 브랜치 생성");
+        System.out.println("TODO 브랜치 TEST");
     }
 }
