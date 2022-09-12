097f6d57f5acb4923ba51997be6f76dfebbf4906
jyang <did3706@naver.com>
2022-09-12 18:08:45 +0900

    todo 브랜치생성

--- java/todo-server/src/main/java/com/example/todoserver/TodoServerApplication.java
+++ java/todo-server/src/main/java/com/example/todoserver/TodoServerApplication.java
@@ -3,5 +3,6 @@ package com.example.todoserver;
 public class TodoServerApplication {
     public static void main(String[] args) {
         System.out.println("HELLO TODO");
+        System.out.println("TODO 브랜치 생성");
     }
 }
