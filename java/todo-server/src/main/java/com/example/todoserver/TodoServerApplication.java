38e2fa41742c30bdc1ac529636ba3abf829584d3
jyang <did3706@naver.com>
2022-09-12 20:02:33 +0900

    Todo 메인

--- java/todo-server/src/main/java/com/example/todoserver/TodoServerApplication.java
+++ java/todo-server/src/main/java/com/example/todoserver/TodoServerApplication.java
@@ -1,5 +1,7 @@
 package com.example.todoserver;
 
+import com.example.todoserver.repository.TodoRepository;
+
 public class TodoServerApplication {
     public static void main(String[] args) {
         System.out.println("TODO 브랜치 사용");
