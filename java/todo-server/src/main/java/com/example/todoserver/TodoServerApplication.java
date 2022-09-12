722ac2507ef4f53d018a38700d043ba41f85cc39
jyang <did3706@naver.com>
2022-09-12 18:30:57 +0900

    todo 브랜치 사용

--- java/todo-server/src/main/java/com/example/todoserver/TodoServerApplication.java
+++ java/todo-server/src/main/java/com/example/todoserver/TodoServerApplication.java
@@ -2,7 +2,6 @@ package com.example.todoserver;
 
 public class TodoServerApplication {
     public static void main(String[] args) {
-        System.out.println("HELLO TODO");
-        System.out.println("TODO 브랜치 TEST");
+        System.out.println("TODO 브랜치 사용");
     }
 }
